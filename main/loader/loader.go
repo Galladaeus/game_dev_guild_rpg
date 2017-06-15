aa9d7f481266e6ab35bc45dcf4b264efcfb79683
Titus Rasmussen <titus.rasmussen@gmail.com>
2017-06-14 19:59:33 -0700

    Ability to switch between two "levels" added

---
+++ main/loader/loader.go
@@ -0,0 +1,51 @@
+components {
+  id: "script"
+  component: "/main/loader/loader.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "battle"
+  type: "collectionproxy"
+  data: "collection: \"/battle/battle.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "town"
+  type: "collectionproxy"
+  data: "collection: \"/town/town.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
