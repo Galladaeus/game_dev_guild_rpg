e2cbe77cc41ecb85d6ec2cb1e0b9475f89847b4a
Galladaeus <galladaeus@hotmail.com>
2018-01-10 16:06:44 -0500

    Made it so quests in quest list move up if a quest higher on the quest list gets completed, also added an inventory, cannot be closed yet, and also does not have any uses yet

---
+++ user_interfaces/inventory_ui/inventory.go
@@ -0,0 +1,15 @@
+components {
+  id: "inventoryGUI"
+  component: "/user_interfaces/inventory_ui/inventoryGUI.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
